f94b4104f1a771527ee926473f3172b7f7e2e432
Alexey Savchuk <sabo@devpreview.ru>
2017-07-22 12:41:34 +0300

    Removed reflect-metadata from dependencies

--- dist/ObjectMapper.es2015.js
+++ dist/ObjectMapper.es2015.js
@@ -1,5 +1,3 @@
-import 'reflect-metadata';
-
 function __decorate(decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
