caf15eb1b012ed85fed7d5fc09fe780284692399
Shakil Siraj <shakil.siraj@gmail.com>
2018-01-23 00:58:56 +1100

    Release 1.5.2

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,9 @@ import typescript from 'rollup-plugin-typescript';
 
 export default {
     entry: './src/main/index.ts',
-    dest: './dist/ObjectMapper.es2015.js',
-    format: 'es',
+    dest: './dist/ObjectMapper.js',
+    format: 'cjs',
+    sourceMap: true,
     external: [
         'reflect-metadata'
     ],
