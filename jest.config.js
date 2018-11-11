718c8f8697080bdf7eb44701e34065b87be08e9c
EC2 Default User <ec2-user@ip-172-31-15-100.us-east-2.compute.internal>
2018-11-11 02:05:03 +0000

    more jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "globals": {
         "ts-jest": {
             "diagnostics": true,
-            "tsConfig": "./tsconfig.spec.json"
+            "tsConfig": "<rootDir>/tsconfig.spec.json"
         },
     },
     "testPathIgnorePatterns": [
